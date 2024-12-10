e1fdd5398b19565d74aa7762a428ebf04d337ab5
proMahadi <professionalmahadi@gmail.com>
2024-12-10 22:51:05 +0600

    add to todo done. design update delete feature will be added soon

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let addTask = document.querySelector("#addTask");
+let taskInput = document.querySelector("#taskInput");
+let taskList = document.querySelector("#task-list");
+let tasks = [];
+
+const newTask = () => {
+    let text = taskInput.value
+    if(text){
+        tasks.push({
+            text:text,
+            completed:false
+        });
+        taskInput.value = "";
+        updateTaskList();
+    }
+};
+
+const updateTaskList = () =>{
+    taskList.innerHTML = "";
+    tasks.forEach(task =>{
+        const listItem = document.createElement('li')
+
+        listItem.innerHTML = `<div>
+        ${task.text}
+        </div>`
+
+        taskList.append(listItem)
+    })
+}
+
+addTask.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  newTask()
+});
